bbc3436f82aa322db5638c7983437c9a8d1c3f58
ingridlopez45 <ilopezo3@miumg.edu.gt>
2024-04-19 01:01:55 -0600

    Update login page name to welcome page and main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gibu/pages/login_page.dart';
+import 'package:gibu/pages/welcome_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: WelcomePage(),
     );
   }
 }

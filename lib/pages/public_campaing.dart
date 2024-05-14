baf204eee48801e7994215b936183f7d8b1a7a1f
ingridlopez45 <ilopezo3@miumg.edu.gt>
2024-05-13 18:57:22 -0600

    Refactor campaign_structure.dart and public_campaing.dart. Add campaign related images.

--- lib/pages/public_campaing.dart
+++ lib/pages/public_campaing.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gibu/components/campaing_structure.dart';
 
 class PublicCampaing extends StatelessWidget {
-  PublicCampaing({super.key});
+  const PublicCampaing({super.key});
 
   @override
   Widget build(BuildContext context) {

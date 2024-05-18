010595295270f521f864ae4699923b806268b136
Manny-Munoz <mhernandezm92@miumg.edu.gt>
2024-05-18 12:30:31 -0600

    feat: Add SettingsPage to MainPage and update navigation

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,17 +4,19 @@ class Button extends StatelessWidget {
   final String text;
   final Color textColor;
   final Color buttonColor;
+  final double buttonWidth;
+  final double buttonHeight;
   final void Function()? onTap;
 
-  const Button({super.key, this.text = "Login", this.textColor = const Color.fromARGB(255, 230, 245, 196), this.buttonColor = const Color.fromARGB(255, 23, 77, 77), required this.onTap});
+  const Button({super.key, this.text = "Login", this.textColor = const Color.fromARGB(255, 230, 245, 196), this.buttonColor = const Color.fromARGB(255, 23, 77, 77), this.buttonWidth = 350, this.buttonHeight = 75, required this.onTap, });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 350,
-        height: 75,
+        width: buttonWidth,
+        height: buttonHeight,
         decoration: BoxDecoration(
           color: buttonColor,
           borderRadius: BorderRadius.circular(10),
--- lib/components/campaing_structure.dart
+++ lib/components/campaing_structure.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gibu/components/double_to_currency.dart';
 import 'package:gibu/components/tag.dart';
 import 'package:gibu/components/progress_bar.dart';
+import 'package:gibu/components/button.dart';
 
-class CampaingStructure extends StatelessWidget {
+class CampaingStructure extends StatefulWidget {
   final String campaingTitle;
   final String imagePath;
   final String heroPath;
@@ -23,75 +24,98 @@ class CampaingStructure extends StatelessWidget {
   });
 
   @override
+  State<CampaingStructure> createState() => _CampaingStructureState();
+}
+
+class _CampaingStructureState extends State<CampaingStructure> {
+  bool _isFavorite = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-        child: Container(
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 240, 236, 236),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            margin: const EdgeInsets.symmetric(vertical: 20),
             color: const Color.fromARGB(255, 240, 236, 236),
             child: SafeArea(
                 child: SingleChildScrollView(
                     child: Column(children: [
               const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  Image.asset("lib/images/favorite.png"),
-                ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text(
+                      widget.campaingTitle,
+                      style: const TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            _isFavorite = !_isFavorite;
+                          });
+                        },
+                        child: Image.asset(_isFavorite
+                            ? "lib/images/heart-circle.png"
+                            : "lib/images/heart-circle-favorite.png"),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              const SizedBox(height: 7),
-              Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                  child: Text(
-                    campaingTitle,
-                    style: const TextStyle(color: Colors.black, fontSize: 18),
-                  )),
               const SizedBox(height: 10),
-              Image.asset(imagePath),
+              Image.asset(widget.imagePath),
               const SizedBox(height: 8),
-              Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 23.0),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            const SizedBox(width: 3),
-                            Image.asset(heroPath),
-                            const SizedBox(width: 8),
-                            Text(
-                                "$fundraiserName is organizing this fundraiser",
-                                style: const TextStyle(
-                                    fontSize: 10,
-                                    color: Color.fromARGB(255, 104, 104, 104))),
-                          ],
-                        ),
-                        const Tag()
-                      ])),
-              const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.only(
-                    right: 32.0, left: 32.0, top: 10.0, bottom: 10.0),
-                child: Column(
+              const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ProgressBar(goal: goal, raised: raised),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Raised: ${doubleToCurrency(raised)} of ${doubleToCurrency(goal)} goal",
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+                    Flexible(child: Text("2 people just donated")),
+                    Tag()
+                  ]),
               const SizedBox(height: 8),
+              Column(
+                children: [
+                  ProgressBar(goal: widget.goal, raised: widget.raised),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Raised: ${doubleToCurrency(widget.raised)}",
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      Text("Goal: ${doubleToCurrency(widget.goal)}",
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Fundraiser",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                  Row(
+                    children: [
+                      Image.asset(widget.heroPath),
+                      const SizedBox(width: 8),
+                      Text(widget.fundraiserName,
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 104, 104, 104))),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
               Container(
-                width: 350,
+                width: double.infinity,
                 decoration: BoxDecoration(
                   border: Border.all(
                       color: const Color.fromARGB(255, 182, 182, 182),
@@ -103,7 +127,7 @@ class CampaingStructure extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(
                       horizontal: 23.0, vertical: 10.0),
                   child: Text(
-                    "$description ",
+                    "${widget.description} ",
                     style: const TextStyle(
                         color: Color.fromARGB(255, 104, 104, 104),
                         fontSize: 14),
@@ -111,6 +135,15 @@ class CampaingStructure extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 12),
+              Button(
+                text: "Donate Now",
+                buttonWidth: double.infinity,
+                buttonHeight: 50,
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  '/main',
+                ),
+              ),
             ])))));
   }
 }
